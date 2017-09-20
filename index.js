e893e1685b6b034dcea66b544becee93a8c4aa91
Julio Torres <jtorres1989@gmail.com>
2017-09-20 20:45:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function sayHiToGrandma(string){
   if string.toLowerCase() === string
   return ("I can't hear you!")
 }
-function sayHiToGrandma(shout){
+function sayHiToGrandma(string){
   if string.toUpperCase() === string
   return ("YES INDEED!")
 }
