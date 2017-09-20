ac61d96b7074413e162d66a5c81f5d871db59084
Julio Torres <jtorres1989@gmail.com>
2017-09-20 20:46:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,8 +13,6 @@ function logWhisper(string){
 function sayHiToGrandma(string){
   if string.toLowerCase() === string
   return ("I can't hear you!")
+  else ("YES INDEED")
 }
-function sayHiToGrandma(string){
-  if string.toUpperCase() === string
-  return ("YES INDEED!")
-}
+
