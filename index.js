505ecee81ad43de27fe4e4bb4c8c9b7eff52ce96
Julio Torres <jtorres1989@gmail.com>
2017-09-20 19:21:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,7 @@ function logShout(string){
 function logWhisper(string){
   console.log(string.toLowerCase())
 }
+function sayHitToGrandma(string){
+  return string("I can't hear you!")
+  if string.toLowerCase() === lowercase
+}
